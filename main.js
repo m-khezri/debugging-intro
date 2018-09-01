333f716c2cafbfaddf4a980886ae44029ec41daf
mohammad <khezri@outlook.com>
2018-09-01 12:29:50 -0500

    finishing debug

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 console.log('hello');
+
+// const name ='fred'
+
+// if (name ==='fred'){
+// console.log('Hello fred');
+
+// }
+
+const colors = ["Red", "Orange", "Periwinkle", "Goldenrod", "Tomato", "Dodger Blue"]
+
+for (let i = 0; i <= colors.length; i++) {
+    console.log(colors[0]);
+}
